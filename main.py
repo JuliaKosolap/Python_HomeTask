2ef203d8035113e90f1db56fac104f49fd8cc2c0
user <jkosolap@gmail.com>
2023-09-15 22:31:04 +0300

    Task3 implemented

--- main.py
+++ main.py
@@ -62,18 +62,83 @@
 # else: print("Invalid value was provided")
 
 
-#Task2.2
-number = int(input("Enter number of meters: "))
-unit = (input("Enter an unit to which you want to convert your meters (miles, inches or yards): "))
+# #Task2.2
+# number = int(input("Enter number of meters: "))
+# unit = (input("Enter a unit to which you want to convert your meters (miles, inches or yards): "))
+#
+# miles = number / 1609.344
+# inches = number * 39.3701
+# yards = number * 1.09361
+#
+# if unit == "miles":
+#     print(miles)
+# elif unit == "inches":
+#     print(inches)
+# elif unit == "yards":
+#     print(yards)
+# else: print ("Invalid value was provided")
 
-miles = number / 1609.344
-inches = number * 39.3701
-yards = number * 1.09361
 
-if unit == "miles":
-    print(miles)
-elif unit == "inches":
-    print(inches)
-elif unit == "yards":
-    print(yards)
-else: print ("Invalid value was provided")
+# Task3.1
+try:
+    num = int(input("Enter number from 1 to 7: "))
+
+    match num:
+        case 1:
+            print("Monday")
+        case 2:
+            print("Tuesday")
+        case 3:
+            print("Wednesday")
+        case 4:
+            print("Thursday")
+        case 5:
+            print("Friday")
+        case 6:
+            print("Saturday")
+        case 7:
+            print("Sunday")
+        case _:
+            print("Invalid number!")
+
+except ValueError as e:
+    print("Value should be only number from 1 to 7!")
+
+# Task 3.2
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+
+    if num1 == num2:
+        print("Numbers are the same")
+    elif num1 < num2:
+        print(num1)
+        print(num2)
+    else:
+        print(num2)
+        print(num1)
+
+except ValueError as e:
+    print("Value should be a number only")
+
+
+# Task3.3
+try:
+    num1 = int(input("Enter first number: "))
+    num2 = int(input("Enter second number: "))
+    operation = (input("Enter math operation: "))
+
+    match operation:
+        case "+":
+            print(num1 + num2)
+        case "-":
+            print(num1 - num2)
+        case "*":
+            print(num1 * num2)
+        case "/":
+            print(num1 / num2)
+        case _:
+            print("No such operation!")
+except ValueError as e:
+    print("Provided value was incorrect")
+
